b9704f39a9b65bf99c58d26c378a3169f3ead130
Connie <c.amer@icloud.com>
2021-09-27 21:07:05 +0100

    fix bugs

--- Assets/playerController.cs
+++ Assets/playerController.cs
@@ -4,24 +4,24 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
+    public float speed = 5.0f;
 
-    [SerializeField] private float speed = 10;
-    [SerializeField] private float maxMovement = 10;
+    public GameObject ball;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("hello world");
+        Rigidbody rb = ball.GetComponent<Rigidbody>();
+	rb.AddForce((Vector3.up + Vector3.right) * 300);
     }
 
     // Update is called once per frame
     void Update()
     {
-        float input = Input.GetAxis("Horizontal");
-	
-	float oldX = transform.position.x;
-	float newX = oldX + input;
-
-	transform.position = new Vector3(newX, 0.0f, 0.0f);
-
+        float inp = Input.GetAxis("Horizontal") * Time.deltaTime;
+        float current_x = transform.position.x;
+        float new_x = Mathf.Clamp(current_x + inp, -5.0f, 5.0f);
+        transform.position = new Vector3(new_x, transform.position.y, transform.position.z);
+    
+    	
     }
 }
